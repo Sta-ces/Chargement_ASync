2a0731305157ebdd124ad82b722be34dfcab18cb
Staces <staescedric@gmail.com>
2018-01-08 16:36:03 +0100

    finish

--- Assets/_Project/Scripts/LoadingWorld.cs
+++ Assets/_Project/Scripts/LoadingWorld.cs
@@ -8,16 +8,16 @@ public class LoadingWorld : MonoBehaviour {
     public List<int> m_loadLevel = new List<int>();
     public List<int> m_unloadLevel = new List<int>();
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         foreach(int level in m_loadLevel)
         {
-            if(!SceneManager.GetSceneAt(level).isLoaded)
+            if(!SceneManager.GetSceneByBuildIndex(level).isLoaded)
                 SceneManager.LoadSceneAsync(level, LoadSceneMode.Additive);
         }
         foreach(int level in m_unloadLevel)
         {
-            if (!SceneManager.GetSceneAt(level).isLoaded)
+            if (SceneManager.GetSceneByBuildIndex(level).isLoaded)
                 SceneManager.UnloadSceneAsync(level);
         }
     }
